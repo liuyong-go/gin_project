a9d878530addadb4d0f85258b03f1e4e2cadccaa
liuyong <liuyong@soyoung.com>
2021-11-23 18:22:26 +0800

    配置

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"flag"
 
+	"github.com/liuyong-go/gin_project/bootstrap"
+	"github.com/liuyong-go/gin_project/config"
+	"github.com/toolkits/pkg/runner"
+)
+
+var (
+	f *string
+)
+
+func init() {
+	f = flag.String("f", "", "config path")
+	flag.Parse()
+	runner.Init()
+	config.InitBaseInfo()
+	if *f != "" {
+		config.BaseInfo.ConfigPath = *f
+	}
+
+}
+
+//接收配置参数设置一些全局变量
+//调用bootstrap启动项目
+func main() {
+	bootstrap.Start()
 }
